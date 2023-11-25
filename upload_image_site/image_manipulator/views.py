b0299bcced8d87b06cc236b5b124203eed3578ec
MichalOrdon <AeroRD.Craft@gmail.com>
2023-11-25 23:18:05 +0100

    Rozwinięcie strony głównej o listę plików

--- upload_image_site/image_manipulator/views.py
+++ upload_image_site/image_manipulator/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.conf import settings
+import os
 
 
 def home(request):
-    return render(request, 'image_manipulator/home.html')
+    folder = 'assets/images/'
+    folder_path = os.path.join(settings.BASE_DIR, folder)
+
+    if os.path.exists(folder_path):
+        list_of_files = os.listdir(folder_path)
+        images = [file for file in list_of_files if file.endswith(('jpg', 'jpeg', 'png', 'gif'))]
+    else:
+        images = []
+
+    return render(request, 'image_manipulator/home.html', {'images': images})
